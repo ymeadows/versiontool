e85db3d43bcfbf19945045c871628f24413c99f1
Judson Lester <nyarly@gmail.com>
2021-09-15 13:45:07 -0700

    Wrangling GHA

--- github-action.sh
+++ github-action.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
+set -e
 prefix=""
-if [ -n "$INPUT_prefix" ]; then
-  prefix="--prefix=$INPUT_prefix"
+if [ -n "$INPUT_PREFIX" ]; then
+  prefix="--prefix=$INPUT_PREFIX"
 fi
 
 strict=""
-if [ "$INPUT_strict" == true ]; then
+if [ "$INPUT_STRICT" == true ]; then
   strict="--strict"
 fi
 
-if [ "$INPUT_operation" == "sort" ] || [ "$INPUT_operation" == "highest" ]; then
+if [ "$INPUT_OPERATION" == "sort" ] || [ "$INPUT_OPERATION" == "highest" ]; then
   set -x
-  echo "::set-output name=result::"$( echo "$INPUT_version" | ./versiontool "$INPUT_operation" $INPUT_flags)
-  exit $?
+  echo "::set-output name=result::"$( echo "$INPUT_VERSION" | /versiontool "$INPUT_OPERATION" $INPUT_FLAGS)
 fi
 set -x
-echo "::set-output name=result::"$(./versiontool $prefix $strict "$INPUT_operation" $INPUT_flags "$INPUT_version")
-exit $?
+echo "::set-output name=result::"$(/versiontool $prefix $strict "$INPUT_OPERATION" $INPUT_FLAGS "$INPUT_VERSION")
