6e9e9b3a97797562cf11b8136156b9796e56c451
Judson Lester <nyarly@gmail.com>
2021-09-15 11:47:17 -0700

    GHA inputs and outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,9 @@ fi
 
 if [ "$INPUT_operation" == "sort" ] || [ "$INPUT_operation" == "highest" ]; then
   set -x
-  echo "$INPUT_version" | ./versiontool "$INPUT_operation" $INPUT_flags
+  echo "::set-output name=result::"$( echo "$INPUT_version" | ./versiontool "$INPUT_operation" $INPUT_flags)
   exit $?
 fi
 set -x
-exec ./versiontool $prefix $strict "$INPUT_operation" $INPUT_flags "$INPUT_version"
+echo "::set-output name=result::"$(./versiontool $prefix $strict "$INPUT_operation" $INPUT_flags "$INPUT_version")
+exit $?
